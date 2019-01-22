c45ca8fdebdef865e6c1248cf8486bdace8e6a35
muiscript <myam0118@gmail.com>
2019-01-22 13:34:40 +0900

    in the middle of implementation of lexer

---
+++ lexer/lexer.go
@@ -0,0 +1,50 @@
+package lexer
+
+import (
+	"github.com/muiscript/ether/token"
+	"go/constant"
+	"strings"
+)
+
+type Lexer struct {
+	input           string
+	currentPosition int
+	peekPosition    int
+	currentLine     int
+	ch              byte
+}
+
+func NewLexer(input string) *Lexer {
+	lexer := &Lexer{input: input}
+
+	return lexer
+}
+
+func (l *Lexer) NextToken() token.Token {
+	l.skipSpaces()
+}
+
+// replace \t and \r to white space.
+// after executing this function, lexer should have
+// only white space(' ') and new line('\n') as space characters.
+func (l *Lexer) sanitizeInput() {
+	sanitized := strings.Replace(l.input, "\t", " ", -1)
+	sanitized = strings.Replace(sanitized, "\r", " ", -1)
+	l.input = sanitized
+}
+
+func (l *Lexer) consumeChar() {
+	if l.peekPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.peekPosition]
+	}
+	l.currentPosition = l.peekPosition
+	l.peekPosition++
+}
+
+func (l *Lexer) skipSpaces() {
+	if l.ch == ' ' || l.ch == '\t' || l.ch == '\r' {
+		l.consumeChar()
+	}
+}
